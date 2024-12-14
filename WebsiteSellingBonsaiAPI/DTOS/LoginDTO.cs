a18d6f3ebffa7b0558d653fa7e4549558a38b7f1
Zurika321 <conan17032004@gmail.com>
2024-12-15 00:44:18 +0700

    tách dự án gốc và api ra làm 2 dự án riêng

--- WebsiteSellingBonsaiAPI/DTOS/LoginDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebsiteSellingBonsaiAPI.DTOS
-{
-    public class LoginDTO
-    {
-        public required string Password { get; set; }
-        public string? Username { get; set; }
-        public bool RememberMe { get; set; }
-    }
-}
